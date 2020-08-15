f66c1123f24767af486c7330153c79a9bb74e3b8
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2020-08-15 19:38:31 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function GitHubUser() {
+  const [data, setData] = useState(null);
+  const [name, setName] = useState("github");
+  function updateName() {
+    let val = document.querySelector("input[type=text]").value;
+    setName(val);
+  }
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${name}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, [name]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (data) {
+    return (
+      <div className="main">
+        <h2>Name:{data.login}</h2>
+        <img src={data.avatar_url} />
+        <label>
+          Next:
+          <input type="text" placeholder="Enter A Github Username" />
+          <input type="button" value="GO" onClick={updateName} />
+        </label>
+      </div>
+    );
+  }
+
+  return (
+    <div className="main">
+      <h1>Loading</h1>
+    </div>
+  );
+}
+
+function App() {
+  return <GitHubUser />;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
